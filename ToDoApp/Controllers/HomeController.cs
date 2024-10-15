51aa0b27e24aadaeb19a326896e1ee46948d6b8d
Onur.Nergiz <Onur.Nergiz@ONURNERGIZ>
2024-10-15 11:25:28 +0300

    Add project files.

---
+++ ToDoApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        DatabaseContext databaseContext = new DatabaseContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(databaseContext.Tasks.ToList());
+        }
+
+        public ActionResult TaskAdd(string taskText)
+        {
+            Task task = new Task() { TaskText=taskText};
+            databaseContext.Tasks.Add(task);
+            databaseContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteTask(string taskId)
+        {
+            
+            foreach (Task task in databaseContext.Tasks)
+            {
+                if (task.Id == int.Parse(taskId))
+                {
+                    databaseContext.Tasks.Remove(task); 
+                }
+            }
+            databaseContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ToDoApp/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string TaskText { get; set; }
+    }
+}
